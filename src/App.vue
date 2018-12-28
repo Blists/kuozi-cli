828e5ae5a14c9ec113f186e78029cf124793f52f
xuwenchao <415690116@qq.com>
2018-12-28 17:01:08 +0800

    跳转index使用replace

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@ export default {
             sessionStorage.setItem("$path", "");
         }
         sessionStorage.setItem("$path", path);
-        this.$router.replace({ name: "index" });
+        this.$router.replace({ name: "index", query: { forwardReplace: true } });
     },
     computed: {
         spinner() {
